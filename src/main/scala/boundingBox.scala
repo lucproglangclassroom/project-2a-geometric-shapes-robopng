15a73a7dccbd74027b502d371bf4cf5cb3921477
robopng <109000271+robopng@users.noreply.github.com>
2025-10-20 20:17:09 -0500

    Initial implementations of BoundingBox

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -1,12 +1,15 @@
 package edu.luc.cs.laufer.cs371.shapes
 
 // TODO: implement this behavior
-// edit
 
 import Shape.*
 
 object boundingBox:
   def apply(s: Shape): Location = s match
+    case r: Rectangle => Location(0, 0, r)
+    case e: Ellipse => Location(0, 0, Rectangle(e.width, e.height))
+    case l: Location => Location(l.x, l.y, apply(l.shape))
+    case g: Group => ???
     case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
 
 end boundingBox
--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -1,10 +1,9 @@
 package edu.luc.cs.laufer.cs371.shapes
 
-/** data Shape = Rectangle(w, h) | Location(x, y, Shape) */
+/** data Shape = Rectangle(w, h) | Ellipse(w, h) | Location(x, y, Shape) | Group(shapes) */
 enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
-  case Location(x: Int, y: Int, shape: Shape)
-  // TODO add missing cases (see test fixtures)
   case Ellipse(width: Int, height: Int)
+  case Location(x: Int, y: Int, shape: Shape)
   case Group(shapes: Shape*)
   
