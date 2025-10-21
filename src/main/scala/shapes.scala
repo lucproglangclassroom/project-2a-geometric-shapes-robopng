ee9aeb7dcb8f4fe871155d14943bb78b1b38d554
robopng <109000271+robopng@users.noreply.github.com>
2025-10-20 20:10:14 -0500

    Add required cases to Shapes

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -5,3 +5,6 @@ enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
   case Location(x: Int, y: Int, shape: Shape)
   // TODO add missing cases (see test fixtures)
+  case Ellipse(width: Int, height: Int)
+  case Group(shapes: Shape*)
+  
