4c2e394007c3aca1524a56e44d465d78303f2412
robopng <109000271+robopng@users.noreply.github.com>
2025-10-20 22:53:34 -0500

    Initial implementations of height, scale, size

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -1,7 +1,4 @@
 package edu.luc.cs.laufer.cs371.shapes
-
-// TODO: implement this behavior
-
 import Shape.*
 
 object boundingBox:
--- src/main/scala/size.scala
+++ src/main/scala/size.scala
@@ -1,3 +1,12 @@
-object size {
+package edu.luc.cs.laufer.cs371.shapes
+import Shape.*
 
-}
+object size:
+  def apply(s: Shape): Int = s match
+    case r: Rectangle => 1
+    case e: Ellipse => 1
+    case l: Location => apply(l.shape)
+    case g: Group => g.shapes.foldLeft(0)((x, y) => x + apply(y))
+    case _ => 0
+
+end size
